433934abaf3c7517ce6a6c0f828392fdcc895ebf
JoshBooker <josh@joshbooker.com>
2021-03-18 15:44:48 -0400

    first commit

---
+++ dbo/Tables/Jobs.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Jobs] (
+    [JobName] NVARCHAR (50) NOT NULL,
+    [RunDate] DATETIME2 (0) NOT NULL,
+    CONSTRAINT [PK__Jobs__056690C203317E3D] PRIMARY KEY CLUSTERED ([JobName] ASC)
+);
+
+
+GO
+
